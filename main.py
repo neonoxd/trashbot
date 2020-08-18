dd31dc8e1206c080349cfd4da79d36bcafaf2192
neonoxd <neonoxd14@gmail.com>
2020-08-18 20:12:21 +0200

    insults txt, refactor, new statuses

--- main.py
+++ main.py
@@ -12,25 +12,10 @@ bot = commands.Bot(command_prefix=cfg["prefix"])
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 PHToken = "92a95ab0a3f4d2de"
-with open('zene.txt', 'r', encoding="utf8") as file:
-    legjob_zene_list = file.read().split("\n\n")
-with open('idle_statuses.txt', 'r', encoding="utf8") as file:
-    statuses = file.read().split("\n")
-
-print(statuses)
-
 
 @bot.command(name='hal')
 async def slap(ctx, *, reason: Slapper):
     await ctx.send(reason)
-
-
-@bot.event
-async def on_message(message):
-    from eventhandlers import handle_on_message
-    if message.author == bot.user:
-        return
-    await handle_on_message(bot, message)
 
 
 @bot.command(name='captcha', description="random PH captcha")
@@ -42,7 +27,8 @@ async def captcha(ctx):
 
 @bot.command(name='zene', description="random leg job zene idézet")
 async def zene(ctx):
-    await ctx.send(random.choice(legjob_zene_list))
+    embed = discord.Embed(description="-Leg job zenék", title=random.choice(shared.legjob_zene_list), color=0xfc0303)
+    await ctx.send(embed=embed)
 
 
 @bot.command(name='arena', description="ketrecharc bunyo, hasznalat: {0}arena @user1 @user2 ...".format(cfg["prefix"]))
@@ -65,13 +51,21 @@ async def captcha(ctx, *args):
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Game(random.choice(statuses)))
+    await bot.change_presence(activity=discord.Game(random.choice(shared.statuses)))
     print("ready")
 
 
 @bot.event
 async def on_typing(channel, user, when):
     from eventhandlers import handle_on_typing
-    await handle_on_typing(bot, channel, user, when, statuses)
+    await handle_on_typing(bot, channel, user, when)
+
+@bot.event
+async def on_message(message):
+    from eventhandlers import handle_on_message
+    if message.author == bot.user:
+        return
+    await handle_on_message(bot, message)
+
 
 bot.run(TOKEN)
