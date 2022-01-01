14293a8d16552ef9d37d86b331fd3e564a9ef8fa
blood <bloodika@gmail.com>
2022-01-01 23:37:04 +0100

    Youtube checker for famous hungarian shit

--- cogs/misc.py
+++ cogs/misc.py
@@ -220,6 +220,32 @@ class MiscCog(commands.Cog):
         part.headers.pop(aiohttp.hdrs.CONTENT_TYPE, None)
         return part
 
+	@command.command(name="status")
+	def get_live_statuses(self,ctx):
+	    import requests
+
+	    class Channel:
+	        def __init__(self,name,url,status=False):
+	            self.name = name
+	            self.url = url
+	            self.status = status
+
+	    searching = '"LIVE"'
+	    cookies = dict(cookies_are='YSC=BfqtpHxcuJo; CONSENT=YES+yt.417698365.en+FX+661;')
+	    headers={"Content-Type":"text/html"}
+	    statuses = []
+
+	    channels_to_check = {
+	        Channel("Sodi","c/SoDIvlogja"),
+	        Channel("Tibi/Merci","c/KaffkaMercédeszofficial"),
+	        Channel("Kozma Csabi","c/YoutubePrófétaKozmaLászlóCsaba"),
+	        Channel("Janka","channel/UCBhxAavhPaGbQsEY5dqhaMw"),
+	        Channel("Híradó","channel/UCHJ8gW2vKH5R3VohymtBCrQ")
+	    }
+	    for a in channels_to_check:
+	        response = requests.get(f"https://www.youtube.com/{a.url}",headers=headers,cookies=cookies)
+	        statuses.append(f"{a.name} - {searching in response.content.decode('UTF-8')}")
+		ctx.send("\r\n".join(statuses))
 
 def setup(bot):
     bot.add_cog(MiscCog(bot))
